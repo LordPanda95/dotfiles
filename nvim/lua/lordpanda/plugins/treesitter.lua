533a5a51b6564eb355b896c9540b9d065289508f
LordPanda <FFVIICCSephiroth@gmail.com>
2024-05-05 03:13:54 +0300

    disable unused option

--- nvim/lua/lordpanda/plugins/treesitter.lua
+++ nvim/lua/lordpanda/plugins/treesitter.lua
@@ -61,10 +61,10 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
+          init_selection = false,
+          node_incremental = false,
           scope_incremental = false,
-          node_decremental = "<bs>",
+          node_decremental = false,
         },
       },
     })
