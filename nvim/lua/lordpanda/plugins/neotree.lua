2f9df20d1c5748971b2beb4abdb431d1fd424260
LordPanda <FFVIICCSephiroth@gmail.com>
2024-05-08 12:35:23 +0300

    formating file

--- nvim/lua/lordpanda/plugins/neotree.lua
+++ nvim/lua/lordpanda/plugins/neotree.lua
@@ -1,36 +1,33 @@
-return{
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function ()
-        vim.fn.sign_define("DiagnosticSignError",
-            {text = " ", texthl = "DiagnosticSignError"})
-        vim.fn.sign_define("DiagnosticSignWarn",
-          {text = " ", texthl = "DiagnosticSignWarn"})
-        vim.fn.sign_define("DiagnosticSignInfo",
-          {text = " ", texthl = "DiagnosticSignInfo"})
-        vim.fn.sign_define("DiagnosticSignHint",
-          {text = "󰌵", texthl = "DiagnosticSignHint"})
-        require("neo-tree").setup({
-            filesystem = {
-                filtered_items = {
-                    visible = true,
-                    show_hidden_count = true,
-                    hide_dotfiles = false,
-                    hide_gitignored = false,
-                    hide_by_name = {
-                        '.git',
-                        '.DS_Store',
-                        'thumbs.db',
-                    },
-                    never_show = {},
-                },
-            }
-        })
-    end,
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+    require("neo-tree").setup({
+      close_if_last_window = false,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+            "thumbs.db",
+          },
+          never_show = {},
+        },
+      },
+    })
+  end,
 }
