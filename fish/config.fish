d1cf460ead456afd8d322dd02b9685af3e13e4a8
LordPanda <FFVIICCSephiroth@gmail.com>
2024-05-09 13:59:47 +0300

    clean fish config

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,6 @@ set -gx BAT_THEME "Dracula"
 set fzf_preview_dir_cmd exa -alg
 set fzf_fd_opts --hidden --max-depth 5
 set fzf_diff_highlighter delta
-#set fzf_configure_bindings --history=\c\r
 
 source "$HOME/.cargo/env.fish"
 
@@ -18,7 +17,5 @@ set -gx EDITOR nvim
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_EN.UTF-8
 set -gx LANGUAGE en_US.UTF-8
-
-#fzf --fish | source
 
 source /Users/lordpanda/.config/op/plugins.sh
