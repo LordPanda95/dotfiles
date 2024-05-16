4906b695da50ed37801cf39ab0ebb9d6c8a769fe
LordPanda <FFVIICCSephiroth@gmail.com>
2024-05-16 12:50:05 +0300

    add ai extension

--- nvim/lua/lordpanda/plugins/nvim-cmp.lua
+++ nvim/lua/lordpanda/plugins/nvim-cmp.lua
@@ -55,13 +55,16 @@ return {
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
         { name = "dotenv" },
+        { name = "codeium" }, -- ai code
       }),
 
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
+          --[[ mode = "symbol", ]]
           maxwidth = 50,
           ellipsis_char = "...",
+          symbol_map = { Codeium = "" },
         }),
       },
     })
